196a527998b0b38e494f424daf30640822859f84
Ann Siapno <ann.n.siapno@gmail.com>
2017-08-05 19:46:40 +1000

    added index view

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,26 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
+
+from .models import Question
 
 def index(req):
-    return HttpResponse("You are at the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(req, 'polls/index.html', context)
+
+def detail(req, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesnotExist:
+        raise Http404("Question does not exist")
+    return render(req, "polls/detail.html", {'question': question})
+
+def results(req, question_id):
+    response = "Your're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(req, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
