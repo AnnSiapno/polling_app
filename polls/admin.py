c546314ff200a6fae59ffccb28718c8cfcc2040b
Ann Siapno <ann.n.siapno@gmail.com>
2017-08-06 18:52:22 +1000

    changed admin view

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
 
-# Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['pub_date']}),
+        (None, {'fields': ['question_text']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+admin.site.register(Question, QuestionAdmin)
